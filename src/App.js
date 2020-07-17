6f7fc2f82a0a93cd417890e768bddbca02fdace5
Daniel Donovan <spitfiredd@gmail.com>
2020-07-17 16:14:40 -0700

    hooked up basic routing for bike intake form

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import React, { useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';  
+// import logo from './logo.svg';
 import './App.css';
+import BikeForm from './components/BikeForm';
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time)
-    });
-  }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
-  );
+class App extends Component {  
+  render() {
+    return (
+      <div className="container">
+        <BikeForm />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/BikeForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+
+async function postData(endpoint = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(endpoint, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+class BikeForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      color: '',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    const { name, color } = this.state;
+    // let bikeData = this.state;
+    let bikeData = {
+      "name": name,
+      "color": color,
+    }
+
+    postData('api/v1/bikes', bikeData)
+      .then(data => {
+        console.log(data)
+      });
+
+  }
+
+  render() {
+    const { name, color } = this.state;
+
+    return (
+      <div>
+        <h3>Bike Intake Form</h3>
+        
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={ name }
+            onChange={ this.onChange }
+          />
+          <input
+            type="test"
+            name="color"
+            value={ color }
+            onChange={ this.onChange }
+          />
+          <button type="submit">Submit</button>
+        </form>
+      
+      </div>
+    );
+  }
+}
+
+export default BikeForm;
